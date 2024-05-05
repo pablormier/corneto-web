3edac0067378616a34abf200738e1374e2cc948b
Pablo R. Mier <pablo.rodriguez.mier@gmail.com>
2024-05-05 16:57:28 +0200

    Fix viz.py

--- content/viz.py
+++ content/viz.py
@@ -1,4 +1,4 @@
-def render_dot(dot_input, container_id=None):
+def render_dot(dot_input, container_id=None, vizjs_version="2.1.2"):
     import base64
     import uuid
     from IPython.display import display, HTML
@@ -29,24 +29,27 @@ def render_dot(dot_input, container_id=None):
     # Base64 encode the DOT content to safely embed it in HTML/JavaScript
     dot_string_base64 = base64.b64encode(dot_string.encode()).decode('utf-8')
 
-    # Create the HTML code to display the graph
     html_code = f"""
-    <div id='{container_id}'></div>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/viz.js/2.1.2/viz.js'></script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/viz.js/2.1.2/full.render.js'></script>
+    <div id='{container_id}'></div> <!-- Container to display the graph -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/viz.js/{vizjs_version}/viz.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/viz.js/{vizjs_version}/full.render.js'></script>
     <script>
-    (function() {{
+    function renderGraph(encodedDot) {{
         var viz = new Viz();
-        var encodedDot = "{dot_string_base64}";
+        // Decode the base64 string to get the DOT format
         var dotString = atob(encodedDot);
+        
         viz.renderSVGElement(dotString)
             .then(function(element) {{
                 document.getElementById('{container_id}').appendChild(element);
             }})
-            .catch(function(error) {{
+            .catch(error => {{
                 console.error('Error rendering graph:', error);
             }});
-    }})();
+    }}
+
+    // Execute the rendering function with the base64-encoded DOT string
+    renderGraph("{dot_string_base64}");
     </script>
     """
 
